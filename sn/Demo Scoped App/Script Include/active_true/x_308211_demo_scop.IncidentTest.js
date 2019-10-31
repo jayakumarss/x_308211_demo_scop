e4c20cc21f9d5d8eef3d2b3cb8f12a9155f304c3
System Administrator <admin@example.com>
2019-10-31 08:11:32 -0400

    Changes by 'System Administrator' on 'Demo Scoped App - 1.0.0' -- Build #1

---
+++ sn/Demo Scoped App/Script Include/active_true/x_308211_demo_scop.IncidentTest.js
@@ -0,0 +1,37 @@
+/* 
+ * Application : Demo Scoped App
+ * ClassName   : sys_script_include
+ * Created On  : 2019-10-31 12:05:51
+ * Created By  : admin
+ * Updated On  : 2019-10-31 12:09:02
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=1d057a02dbbc0010343f58b3ca9619e4
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_308211_demo_scop.module:sys_script_include
+ */
+var Incident_Test = Class.create();
+Incident_Test.prototype = /** @lends x_308211_demo_scop.module:sys_script_include.Incident_Test.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+	/**
+	 * Description
+	 * 
+	 * @returns {boolean}
+	 */
+	dummy: function(){
+		gs.addInfoMessage("Testing devops");
+		return true;
+	},
+    type: 'IncidentTest'
+};

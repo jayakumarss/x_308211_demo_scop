83327510b82984c9afc79568242c323cb6d541c9
System Administrator <admin@example.com>
2019-10-31 08:16:28 -0400

    Changes by 'System Administrator' on 'Demo Scoped App - 1.0.0' -- Build #1

---
+++ sn/Demo Scoped App/Dictionary Entry/x_308211_demo_scop_incidents/active_true/number/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Demo Scoped App
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-10-31 12:02:41
+ * Created By  : admin
+ * Updated On  : 2019-10-31 12:02:41
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=bda47202dbbc0010343f58b3ca961926
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
